855038e85f5c8b6a034533ab7dd9a66e9d11d722
wxz <3337876732@qq.com>
2024-04-13 23:36:39 +0800

    errors is solved

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -22,21 +22,18 @@
 // Execute `rustlings hint errors5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::error;
+use std::error::Error;
 use std::fmt;
 use std::num::ParseIntError;
 
-// TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn ???>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
-    println!("output={:?}", PositiveNonzeroInteger::new(x)?);
+    let positive_nonzero_integer = PositiveNonzeroInteger::new(x).map_err(|e| Box::new(e) as Box<dyn Error>)?;
+    println!("output={:?}", positive_nonzero_integer);
     Ok(())
 }
-
-// Don't change anything below this line.
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -57,7 +54,6 @@ impl PositiveNonzeroInteger {
     }
 }
 
-// This is required so that `CreationError` can implement `error::Error`.
 impl fmt::Display for CreationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let description = match *self {
@@ -68,4 +64,8 @@ impl fmt::Display for CreationError {
     }
 }
 
-impl error::Error for CreationError {}
+impl std::error::Error for CreationError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
